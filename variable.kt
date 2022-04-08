10accbb082597ecddf9097806c2a46300b49c41a
Michele Nardini <michele.nardini3g@gmail.com>
2022-04-08 13:17:42 +0200

    Create variable.kt

---
+++ variable.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var numeroCheCambiaValore = 10
+    numeroCheCambiaValore = 11
+    
+    var stringaBella = "pizza"
+    stringaBella = "pizzetta"
+    
+    /*
+     * quando variabile non essegnata 
+     * bisogna definire il tipo
+    */
+    val nome : String
+    nome = "Miki"
+    
+    //val numeroCheNonCambiaValore = 110
+    //numeroCheNonCambiaValore = 111
+    
+    
+    println("Hello, world!")
+}
