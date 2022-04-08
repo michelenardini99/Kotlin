7051b3a5462c1446e5a118a19601591442cb2d9e
Michele Nardini <michele.nardini3g@gmail.com>
2022-04-08 14:48:04 +0200

    Create Function.kt

---
+++ Function.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println(2+3)
+    println(somma(2, 3))
+}
+
+fun somma(x:Int, y:Int):Int{
+    print("Sto sommando $x con $y...")
+    return x+y
+}
+
+/*
+    fun somma(x:Int, y:Int):Int = x+y
+ */
