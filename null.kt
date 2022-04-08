d155188bdd8e08edef796e6bbd80d80671cae294
Michele Nardini <michele.nardini3g@gmail.com>
2022-04-08 13:21:44 +0200

    Create null.kt

---
+++ null.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var nonVuoto : String = "ciaone"
+    
+    /*
+     * Aggiungere ? d'avanti al tipo di variabile
+     * per definire che può assumere valore null
+    */ 
+    var vuoto : String? = "dopo vuoto"
+    vuoto = null
+    
+    println(vuoto)
+}
